edca2c79a40df5501aaa525435c032f9126897f3
Антон Сободаж <asobodazh@mail.ru>
2022-09-07 17:21:07 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse
+
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Мой блог')
+
+
+# Страница с публикациями друзей
+def group_posts(request, slug):
+    return HttpResponse(f'Публикации друзей {slug}')
